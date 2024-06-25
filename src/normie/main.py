6eaa046fa392efb04d1f4675a7e68b7d711a777d
extreme4all <>
2024-06-25 23:55:22 +0200

    created db structure

---
+++ src/normie/main.py
@@ -0,0 +1,37 @@
+from dataclasses import asdict
+
+from sqlalchemy import (
+    insert,
+    text,
+)
+
+from .. import ActivitiesRecord, BenchmarkABC, HiscoreRecord, SkillsRecord, get_session
+
+
+class BenchMark(BenchmarkABC):
+    def insert_many_records(self, records: list[HiscoreRecord]) -> None:
+        ...
+
+    def get_latest_record_for_player(
+        self,
+        player_id: int,
+    ) -> HiscoreRecord:
+        ...
+
+    def get_latest_record_for_many_players(
+        self,
+        players: list[int],
+    ) -> list[HiscoreRecord]:
+        ...
+
+    def get_all_records_for_player(
+        self,
+        player_id: int,
+    ) -> list[HiscoreRecord]:
+        ...
+
+    def get_all_records_for_many_players(
+        self,
+        players: list[int],
+    ) -> list[HiscoreRecord]:
+        ...
